8f7239bd357ecd17eec1c04f0e6a0a19aa9ef80e
cvdgp <57316995+cvdgp@users.noreply.github.com>
2022-02-13 22:56:30 +0100

    "Modificando eyentes"

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@
 /*
 El pasaporte español es:
 
-3 letras y 6 dígitos y opcionalmente 1 letra (caracter de control para las autoridades)
+3 letras y 6 dígitos y opcionalmente 1 letra (carácter de control para las autoridades)
 La regex seria:
 
 
 Los DNI españoles pueden ser:
 
-NIF (Numero de Identificación Fiscal) - 8 numeros y una letra1
-NIE (Numero de Identificación de Extranjeros) - 1 letra2, 7 numeros y 1 letra1
+NIF (Numero de Identificación Fiscal) - 8 números y una letra1
+NIE (Numero de Identificación de Extranjeros) - 1 letra2, 7 números y 1 letra1
 1 - Una de las siguientes: TRWAGMYFPDXBNJZSQVHLCKE
 2 - Una de las siguientes: XYZ
 
@@ -19,63 +19,78 @@ Pero eso es para comprobar el formato.
 
 A parte, si quieres ser más riguroso, hay una formula para saber si el numero es valido.
 
-Se divide el numero entre 23 y si el resto coincide con la posicion de la letra del documento en la lista TRWAGMYFPDXBNJZSQVHLCKE entonces es valido.
+Se divide el numero entre 23 y si el resto coincide con la posición de la letra del documento en la lista TRWAGMYFPDXBNJZSQVHLCKE entonces es valido.
 
 */
-let documento = document.getElementById("docuemnto");
-let KEY = "TRWAGMYFPDXBNJZSQVHLCKE";
-let documento =  prompt("número documento");
-/*pasmos el dato a string con la letra mayuscula*/
+let btnEnviar 		= document.getElementById("Enviar");
+let txtDni 			= document.getElementById("dni");
+let txtNombre		= document.getElementById("nombre");
+let txtApelido1 	= document.getElementById("apellido1");
+let txtApelido1 	= document.getElementById("apellido1");
+let txtApellido2 	= document.getElementById("apellido2");
 
-/*eL DOCUMENTO  tiene que tener 9 digitos*/
-let DocumentLength = documento.length;
-let lastChart = documento.slice(8,9);
-let numeroDni = parseInt(documento.slice(0,8));
-let letraInicialNie = documento.slice(0,1);
-let tipodocumento = "DNI";
+btnEnviar.addEventListener("click",function(){
+ alert("has hecho submit tarugo");				
+})
 
-/*forzamos para q la ultima y la primera y la combertimos en string letra sea mayuscula*/
-lastChart = 		lastChart.toString().toUpperCase();
-letraInicialNie = 	letraInicialNie.toString().toUpperCase();
+function Validador(){
 
-/*Se divide el numero entre 23 y si el resto coincide con la posicion de la letra del documento en la lista TRWAGMYFPDXBNJZSQVHLCKE entonces es valido */
-let digitoControl =  (lastChart === KEY.charAt(numeroDni % 23) );
-
-/*Evalua que primera letra del NIE una de las siguientes: XYZ en tal caso será un NIE en caso contrario es un DNI*/
-if(letraInicialNie === "Y" || letraInicialNie === "X"  || letraInicialNie === "Z") 
-			tipodocumento = "NIE";
+	let  KEY = "TRWAGMYFPDXBNJZSQVHLCKE";
 
 
+	/*pasmos el dato a string con la letra mayuscula*/
+
+	/*eL DOCUMENTO  tiene que tener 9 digitos*/
+	let DocumentLength = documento.length;
+	let lastChart = documento.slice(8,9);
+	let numeroDni = parseInt(documento.slice(0,8));
+	let letraInicialNie = documento.slice(0,1);
+	let tipodocumento = "DNI";
+
+	/*forzamos para q la ultima y la primera y la combertímos en string letra sea mayuscula*/
+	lastChart = 		lastChart.toString().toUpperCase();
+	letraInicialNie = 	letraInicialNie.toString().toUpperCase();
+
+	/*Se divide el numero entre 23 y si el resto coincide con la posicion de la letra del documento en la lista TRWAGMYFPDXBNJZSQVHLCKE entonces es valido */
+	let digitoControl =  (lastChart === KEY.charAt(numeroDni % 23) );
+
+	/*Evalua que primera letra del NIE una de las siguientes: XYZ en tal caso será un NIE en caso contrario es un DNI*/
+	if(letraInicialNie === "Y" || letraInicialNie === "X"  || letraInicialNie === "Z") 
+				tipodocumento = "NIE";
 
 
 
 
 
-/*si 
-1. el digito control es verdadero y 
-2. son 9 digitos
-3. La primera letra del Nie es uno de "XYZ"
-*/
 
-if(tipodocumento === "NIE"){
-	numeroDni = documento.slice(1,7);
-	digitoControl =  (lastChart === KEY.charAt(numeroDni % 23) );
-	console.log("NIEEEEEEEEEEE");
 
-}if(tipodocumento === "DNI" && digitoControl === true  && DocumentLength === 9 ){
+	/*si 
+	1. el digito control es verdadero y 
+	2. son 9 digitos
+	3. La primera letra del Nie es uno de "XYZ"
+	*/
 
-console.log("ES un dni correcto")
+	if(tipodocumento === "NIE"){
+		numeroDni = documento.slice(1,7);
+		digitoControl =  (lastChart === KEY.charAt(numeroDni % 23) );
+		console.log("NIEEEEEEEEEEE");
+
+	}if(tipodocumento === "DNI" && digitoControl === true  && DocumentLength === 9 ){
+
+	console.log("ES un dni correcto")
+	}
+
+	console.group();
+	console.log("tipo documento  ==>  " + tipodocumento );
+	console.log("longitud Documento ==>  " + DocumentLength);
+	console.log("Ultima letra ==>    " + lastChart);
+	console.log(" Número Dni  ==>   " + numeroDni);
+	console.log("ketra inicial NIE ==>   " + letraInicialNie);
+	console.log("digito control dni ==>   " + digitoControl);
+	console.log("letra correcta es =====>  " + KEY.charAt(numeroDni % 23));
+
+	console.groupEnd();
+
+
+
 }
-
-console.group();
-console.log("tipo documento  ==>  " + tipodocumento );
-console.log("longitud Documento ==>  " + DocumentLength);
-console.log("Ultima letra ==>    " + lastChart);
-console.log(" Número Dni  ==>   " + numeroDni);
-console.log("ketra inicial NIE ==>   " + letraInicialNie);
-console.log("digito control dni ==>   " + digitoControl);
-console.log("letra correcta es =====>  " + KEY.charAt(numeroDni % 23));
-
-console.groupEnd();
-
-
